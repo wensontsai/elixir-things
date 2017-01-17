c9f17ab984dd142c2fa491d4e56f187a25a84a53
Wenson Tsai <wensontsai@gmail.com>
2017-01-16 20:50:04 -0500

    i luzz hot reloading bruuuh

---
+++ discuss/config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  root: Path.dirname(__DIR__),
+  secret_key_base: "gez2j/7G6DtWGOqS+zek9E9Ax9V0VzFwYTq8pmU1otxEhzRgTAYQXA61Ur2NfsaF",
+  render_errors: [accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+config :discuss, ecto_repos: [Discuss.Repo]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+# Configure phoenix generators
+config :phoenix, :generators,
+  migration: true,
+  binary_id: false
